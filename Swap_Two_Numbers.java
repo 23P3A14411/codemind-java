2b9e7049cbbcd7ec5ea0451c94c74965e32ed220
23P31A4411 <147791804+23P3A14411@users.noreply.github.com>
2024-07-31 16:06:10 +0530

    import java.util.*; public class one{     public static void main(String[] arg)     {         Scanner read=new Scanner(System.in);         int a=read.nextInt();         int b=read.nextInt();         System.out.println(b);         System.out.println(a);     } }

---
+++ Swap_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class one{
+    public static void main(String[] arg)
+    {
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        int b=read.nextInt();
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
