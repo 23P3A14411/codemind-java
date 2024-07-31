935740222e7b6daa65ca64266a8502b739bf6186
23P31A4411 <147791804+23P3A14411@users.noreply.github.com>
2024-07-31 15:33:46 +0530

    import java.util.*; public class one{     public static void main(String[] args)     {         int a;         Scanner read = new Scanner(System.in);         a=read.nextInt();         System.out.println(a*(a+1)/2);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class one{
+    public static void main(String[] args)
+    {
+        int a;
+        Scanner read = new Scanner(System.in);
+        a=read.nextInt();
+        System.out.println(a*(a+1)/2);
+    }
+}
