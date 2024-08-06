52395c6873b9c84018d9806c829c301bc6b850f8
23P31A4411 <147791804+23P3A14411@users.noreply.github.com>
2024-08-06 16:00:40 +0530

    import java.util.*; public class one{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int a=read.nextInt();         int b=read.nextInt();         float c=0.5f*a*b;         System.out.printf("%.0f",c);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class one{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        int b=read.nextInt();
+        float c=0.5f*a*b;
+        System.out.printf("%.0f",c);
+    }
+}
